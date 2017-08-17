4cbab8dbc076311dcb0e51157a9571829949ce62
KacperS <kacperstelmach1992@gmail.com>
2017-08-17 05:00:09 +0200

    Add project files.

---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+
+        }
+    }
+}
